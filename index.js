b6a48f2927a86815682c70e2594e0e2e46b06be4
Blossom Fernandes <blossom.fernandes2006@gmail.com>
2025-10-01 20:56:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+
+let count =0;
+score1=document.getElementById("score1-el");
+function add1(){
+    count+=1;
+    score1.innerText=count;
+
+}
+function add2(){
+    count+=2;
+    score1.innerText=count;
+
+}
+function add3(){
+    count+=3;
+    score1.innerText=count;
+
+}
+
+let count1 =0;
+score2=document.getElementById("score2-el");
+function add11(){
+    count1+=1;
+    score2.innerText=count1;
+
+}
+function add21(){
+    count1+=2;
+    score2.innerText=count1;
+
+}
+function add31(){
+    count1+=3;
+    score2.innerText=count1;
+
+}
